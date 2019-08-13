1b1df8420185db64da396055a04adc630f16e446
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-08-13 23:32:07 +0700

    Các kiểu dữ liệu cơ bản trong Java

---
+++ src/HelloWorld/CacKieuDuLieu.java
@@ -0,0 +1,21 @@
+package HelloWorld;
+
+public class CacKieuDuLieu {
+    public static void main(String[] args) {
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Integer.TYPE, Integer.SIZE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Short.TYPE, Short.SIZE, Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Long.TYPE, Long.SIZE, Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Byte.TYPE, Byte.SIZE, Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Float.TYPE, Float.SIZE, Float.MIN_VALUE, Float.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Double.TYPE, Double.SIZE, Double.MIN_VALUE, Double.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n",
+                Character.TYPE, Character.SIZE, Character.MIN_VALUE, Character.MAX_VALUE);
+        boolean isNumber = false; //True hoặc False
+    }
+}
