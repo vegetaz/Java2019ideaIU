c0eea4d36bbfbd03187abff9cc9c14056bb03744
Nguyễn Văn Vận <vegetaz@outlook.com>
2019-08-17 16:45:16 +0700

    Unicode Character

---
+++ src/HelloWorld/UnicodeCharacter.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+/*
+* Unicode Character
+* */
+public class UnicodeCharacter {
+    public static void main(String[] args) {
+        char testUnicode1 = '\u2122';
+        char testUnicode2 = '\u03C0';
+        System.out.println("See this character " + testUnicode1 + " and this character " + testUnicode2);
+    }
+}
